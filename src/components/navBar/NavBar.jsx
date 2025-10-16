cd5ff54f84f888f4f98551b613e43c66c14e0f4d
Md Shihab uddin <shihabfahim4321@gmail.com>
2025-10-16 20:00:42 +0600

    worked on navigation & made it responsive

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "./Link";
+import { Menu, X } from "lucide-react";
 const NavigationData = [
   { id: 1, name: "Home", path: "/" },
   { id: 2, name: "About", path: "/about" },
@@ -9,12 +10,25 @@ const NavigationData = [
 ];
 
 const NavBar = () => {
-  return (
-    <nav>
 
-      <div className="flex">
+  const [open,setOpen] = useState(false);
+
+  const links = NavigationData.map(route => <Link key={route.id} route={route}></Link>)
+  return (
+    <nav className="flex justify-between mx-10">
+
+      <span className="flex" onClick={() => setOpen(!open)}>
+        {open ? <X className="md:hidden"></X> : <Menu className="md:hidden"> </Menu>}
+        
+      <ul className="md:hidden">
+        {links}
+      </ul>
+
+      <h3 className="ml-4">My Navbar</h3>
+      </span>
+      <div className="md:flex hidden">
         {
-        NavigationData.map(route => <Link key={route.id} route={route}></Link>)
+          links
         }
       </div>
       {/* <ul className="flex">
@@ -29,6 +43,8 @@ const NavBar = () => {
                 <li className='mr-10'><a href="">Abput</a></li>
                 <li className='mr-10'><a href="">Blog</a></li>
             </ul> */}
+
+            <button>Sign in</button>
     </nav>
   );
 };
